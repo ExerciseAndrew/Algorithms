97d113b018a0873a1ae5b07f1ac5dbd09c8c738f
Andrew Vaughan <53552952+ExerciseAndrew@users.noreply.github.com>
2022-02-17 09:35:10 -0800

    Create NumberLineJumps.js

---
+++ javascript/NumberLineJumps.js
@@ -0,0 +1,72 @@
+/*
+You are choreographing a circus show with various animals. For one act, you are given
+two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+
+The first kangaroo starts at location  and moves at a rate of  meters per jump.
+The second kangaroo starts at location  and moves at a rate of  meters per jump.
+
+You have to figure out a way to get both kangaroos at the same location at the same 
+time as part of the show. If it is possible, return YES, otherwise return NO.
+*/
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'kangaroo' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+function kangaroo(x1, v1, x2, v2) {
+    if ((v2<v1)&&(x1-x2)%(v2-v1)===0) {
+        return 'YES'
+    }  else {return 'NO'} 
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const x1 = parseInt(firstMultipleInput[0], 10);
+
+    const v1 = parseInt(firstMultipleInput[1], 10);
+
+    const x2 = parseInt(firstMultipleInput[2], 10);
+
+    const v2 = parseInt(firstMultipleInput[3], 10);
+
+    const result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
