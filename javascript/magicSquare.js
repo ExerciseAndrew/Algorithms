3ff6d7aa32144a859501c798bf153c4e04afd67a
Andrew Vaughan <53552952+ExerciseAndrew@users.noreply.github.com>
2022-02-21 14:32:06 -0800

    Create magicSquare.js
    coming back to complete this later

---
+++ javascript/magicSquare.js
@@ -0,0 +1,14 @@
+/*  
+    a magic square is defined as an n x n matrix of distinct
+    positive integers from 1 to n^2
+    where the sum of any row, column, or diagongal of length
+    'n' is always equal to the same number: the magic constant
+    
+    You will be given a 3x3 matrix 's' of integers in that 
+    inclusive range [1,9]. We can convert any digit to any 
+    other digit in the range 1-9 at the cost of a-b
+    Given s, convert it into a magic square at minimul cost.
+    Print this cost on a new line.
+*/
+
+function asserts
