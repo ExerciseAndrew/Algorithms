0c1fdd080fab1dd8d03b5e5c02e4aee58632f46d
Andrew Vaughan <53552952+ExerciseAndrew@users.noreply.github.com>
2022-05-19 10:47:07 -0700

    Update ContainerWithMostWater.js

--- javascript/ContainerWithMostWater.js
+++ javascript/ContainerWithMostWater.js
@@ -7,16 +7,43 @@ Return the maximum amount of water a container can store.
 
 Notice that you may not slant the container.
  */
+// var maxArea = function(height) {
+//     //determine variables for length and width 
+//     let maxVolume = 0;
+    
+//     //iterate through array
+//     for (let i = 0; i<height.length; i++) {
+//         for (let j = height.length-1; j>=0; j--) {
+//             let length = Math.abs(j-i);
+//             // console.log(height[i], 'and', height[j])
+//             maxVolume = Math.max(maxVolume, (length*(Math.min(height[j],height[i]))))
+//         }
+        
+//     }
+    
+//     return maxVolume
+// };
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
 var maxArea = function(height) {
     //determine variables for length and width 
     let maxVolume = 0;
     
     //iterate through array
+    if (height.length===2) {
+        return Math.min(height[0],height[1])
+    }
     for (let i = 0; i<height.length; i++) {
-        for (let j = height.length-1; j>=0; j--) {
-            let length = Math.abs(j-i);
+        for (let j = height.length-1; j>i; j--) {
+            
+            let lol = Math.min(height[j],height[i])
+            let length = j-i;
             // console.log(height[i], 'and', height[j])
-            maxVolume = Math.max(maxVolume, (length*(Math.min(height[j],height[i]))))
+            
+            maxVolume = Math.max(maxVolume, (length*lol))
+            
         }
         
     }
