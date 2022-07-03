e31fcd207ad6ebcc233731d143cf520573c4739e
Andrew Vaughan <53552952+ExerciseAndrew@users.noreply.github.com>
2022-07-02 22:58:29 -0700

    Create maxPathSum.py

---
+++ python/maxPathSum.py
@@ -0,0 +1,27 @@
+def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def max_gain(node):
+            nonlocal max_sum
+            if not node:
+                return 0
+
+            # max sum on the left and right sub-trees of node
+            left_gain = max(max_gain(node.left), 0)
+            right_gain = max(max_gain(node.right), 0)
+            
+            # the price to start a new path where `node` is a highest node
+            price_newpath = node.val + left_gain + right_gain
+            
+            # update max_sum if it's better to start a new path
+            max_sum = max(max_sum, price_newpath)
+        
+            # for recursion :
+            # return the max gain if continue the same path
+            return node.val + max(left_gain, right_gain)
+   
+        max_sum = float('-inf')
+        max_gain(root)
+        return max_sum
